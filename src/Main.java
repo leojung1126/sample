eda8eb27c03c84fdda03b3d0d90e4ef753250783
leojung <leojung1126@gmail.com>
2024-06-19 17:44:25 +0900

    second commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
+		int id = 1;
+		
 		System.out.println("==시작==");
 		
 		Scanner sc = new Scanner(System.in);
@@ -11,14 +13,37 @@ public class Main {
 		while(true) {
 			
 			System.out.printf("명령어)");
+			
 			String cmd = sc.nextLine();
-
+			
 			if (cmd.equals("exit")) {
 				break;
 			}
+
+			if (cmd.equals("article list")) {
+				System.out.println("게시글이 없습니다");
+			}
+			if (cmd.equals("article write")) {
+				
+				System.out.printf("제목 : ");
+				Scanner sc2 = new Scanner(System.in);
+				String cmd2 = sc2.nextLine();
+				
+				System.out.printf("내용 : ");
+				Scanner sc3 = new Scanner(System.in);
+				String cmd3 = sc3.nextLine();
+				
+				System.out.println(id + "가 등록되었습니다");
+				id += 1;
+			
+				System.out.println("");
+			}
 			
 		}
+		
 		sc.close();
+		
+		
 		System.out.println("== 끝 ==");
 	}
 
